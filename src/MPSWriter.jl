5af27c94ea2c2201a7abc26dc290b7f6590d4183
odow <o.dowson@gmail.com>
2016-07-12 22:44:01 +1200

    Bulk of initial work with tests

--- src/MPSWriter.jl
+++ src/MPSWriter.jl
@@ -1,5 +1,206 @@
 module MPSWriter
 
-# package code goes here
+function getrowsense{T1 <: Real, T2<: Real}(rowlb::Vector{T1}, rowub::Vector{T2})
+    @assert length(rowlb) == length(rowub)
+    row_sense = Array(Symbol, length(rowub))
+    hasranged = false
+    for r=1:length(rowlb)
+        @assert rowlb[r] <= rowub[r]
+    	if (rowlb[r] == -Inf && rowub[r] != Inf) || (rowlb[r] == typemin(eltype(rowlb)) && rowub[r] != typemax(eltype(rowub)))
+    		row_sense[r] = :(<=) # LE constraint
+    	elseif (rowlb[r] != -Inf && rowub[r] == Inf)  || (rowlb[r] != typemin(eltype(rowlb)) && rowub[r] == typemax(eltype(rowub)))
+    		row_sense[r] = :(>=) # GE constraint
+    	elseif rowlb[r] == rowub[r]
+    		row_sense[r] = :(==) # Eq constraint
+        elseif (rowlb[r] == -Inf && rowub[r] == Inf)
+            error("Cannot have a constraint with no bounds")
+    	else
+            row_sense[r] = :ranged
+            hasranged = true
+    	end
+    end
+    row_sense, hasranged
+end
+
+function writerows!(io::IO, row_sense::Vector{Symbol})
+    # Objective and constraint names
+    println(io, "ROWS\n N  OBJ")
+    sensechar = ' '
+    for i in 1:length(row_sense)
+        if row_sense[i] == :(<=)
+            sensechar = 'L'
+        elseif row_sense[i] == :(==)
+            sensechar = 'E'
+        elseif row_sense[i] == :(>=)
+            sensechar = 'G'
+        elseif row_sense[i] == :ranged
+            sensechar = 'E'
+        else
+            error("Unknown row sense $(row_sense[i])")
+        end
+        println(io, " $sensechar  C$i")
+    end
+end
+
+function writecolumns!(io::IO, A, colcat, c::Vector, sense::Symbol)
+    @assert sense == :Min || sense == :Max
+    @assert length(colcat) == length(c)
+
+    A = convert(SparseMatrixCSC{Float64, Int32}, A)
+    intgrpOPEN = false
+
+    println(io, "COLUMNS")
+
+    for col in 1:length(c)
+    	if colcat != :nothing
+	        colty = colcat[col]
+	        if colty == :SemiCont || colty == :SemiInt
+                error("The MPS file writer does not currently support semicontinuous or semi-integer variables")
+            end
+	        if (colty == :Bin || colty == :Int) && !_intgrpOPEN
+	            println(io, "    MARKER    'MARKER'                 'INTORG'")
+	            _intgrpOPEN = true
+	        elseif (colty == :Cont || colty == :Fixed) && _intgrpOPEN
+	            println(io, "    MARKER    'MARKER'                 'INTEND'")
+	            _intgrpOPEN = false
+	        end
+	    end
+    	if abs(c[col]) > 1e-10 # Non-zeros
+    		# Flip signs for maximisation
+            println(io, "    V$(rpad(col, 7))  $(rpad("OBJ", 8))  $((sense==:Max?-1:1)*c[col])")
+	    end
+        if length(A.colptr) > col
+            for ind in A.colptr[col]:(A.colptr[col+1]-1)
+            	if abs(A.nzval[ind]) > 1e-10 # Non-zero
+    	            println(io, "    V$(rpad(col, 7))  C$(rpad(A.rowval[ind], 7))  $(A.nzval[ind])")
+    	        end
+            end
+        end
+    end
+    if _intgrpOPEN
+        println(io, "    MARKER    'MARKER'                 'INTEND'")
+    end
+end
+
+function writerhs!(io::IO, rowlb, rowub, row_sense::Vector{Symbol})
+    @assert length(rowlb) == length(rowub) == length(row_sense)
+    println(io, "RHS")
+    for c in 1:length(rowlb)
+        println(io, "    rhs       C$(rpad(c, 7))  $(row_sense[c] == :(<=)?rowub[c]:rowlb[c])")
+    end
+end
+
+function writeranges!(io::IO, rowlb, rowub, row_sense::Vector{Symbol})
+    @assert length(rowlb) == length(rowub) == length(row_sense)
+    println(io, "RANGES")
+    for r=1:length(row_sense)
+        if row_sense[r] == :ranged
+            println(io, "    rhs       C$(rpad(r, 7))  $(rowub[r] - rowlb[r])")
+        end
+    end
+end
+
+function writebounds!(io::IO, collb, colub)
+    @assert length(collb) == length(colub)
+    println(io, "BOUNDS")
+    for col in 1:length(collb)
+        if colub[col] == Inf
+            if collb[col] == -Inf
+                println(io, boundstring("FR", col))
+                continue
+            else
+                println(io, boundstring("PL", col))
+            end
+        else
+            println(io, boundstring("UP", col, colub[col]))
+        end
+        if collb[col] == -Inf
+            println(io, boundstring("MI", col))
+        elseif collb[col] != 0
+            println(io, boundstring("LO", col, collb[col]))
+        end
+    end
+end
+
+function boundstring(ty::ASCIIString, vidx::Int)
+    @assert ty in ["FR", "MI", "PL"]
+    " $ty BOUNDS    V$vidx"
+end
+
+function boundstring(ty::ASCIIString, vidx::Int, val::Real)
+    @assert ty in ["LO", "UP"]
+    " $ty BOUNDS    V$(rpad(vidx, 7))  $(val)"
+end
+
+immutable SOS
+    order::Int
+    indices::Vector{Int}
+    weights::Vector
+end
+
+function addsos!(io::IO, sos::Vector{SOS}, maxvarindex::Int)
+    for i=1:length(sos)
+        @assert length(sos[i].indices) == length(sos[i].weights)
+        println(io, "SOS\n S$(sos[i].order) SOS$(length(sos[i].indices))")
+        for j=1:length(sos[i].indices)
+            @assert sos[i].indices[j] > 0 && sos[i].indices[j] <= maxvarindex
+            println(io, "    V$(rpad(sos[i].indices[j], 7))  $(sos[i].weights[j])")
+        end
+    end
+end
+
+function writeMPS(io::IO,
+    A,                         # the constraint matrix
+    collb::Vector,             # vector of variable lower bounds
+    colub::Vector,             # vector of variable upper bounds
+    c::Vector,                 # vector containing variable objective coefficients
+    rowlb::Vector,             # constraint lower bounds
+    rowub::Vector,             # constraint upper bounds
+    sense::Symbol,             # model sense
+    colcat::Vector,            # constraint types
+    sos::Vector{SOS}=SOS[]     # SOS information
+)
+    # Max width (8) for names in fixed MPS format
+    # TODO: replace names by alpha-numeric
+    #   to enable more variables and constraints.
+    #   Although to be honest no one should be using
+    #   this with that many constraints/variables
+    @assert length(c) <= 9999999
+    @assert length(rowub) <= 9999999
+
+    # Sanity checks
+    @assert length(rowlb) == length(rowub)
+    @assert length(collb) == length(colub) == length(c) == length(colcat)
+    @assert sense == :Min || sense == :Max
+
+    println(io, "NAME          MPSWriter_jl")
+
+    row_sense, hasranged = getrowsense(rowlb, rowub)
+
+    writerows!(io, rowsense)
+    writecolumns!(io, A, colcat, c, sense)
+    if hasranged
+        writeranges!(io, rowlb, rowub, row_sense)
+    end
+    writebounds!(io, collb, colub)
+    addsos!(io, sos, length(collb))
+    println(io, "ENDATA")
+end
+
+function writemps(filename::AbstractString,
+    A,                         # the constraint matrix
+    collb::Vector,             # vector of variable lower bounds
+    colub::Vector,             # vector of variable upper bounds
+    c::Vector,                 # vector containing variable objective coefficients
+    rowlb::Vector,             # constraint lower bounds
+    rowub::Vector,             # constraint upper bounds
+    sense::Symbol,             # model sense
+    colcat::Vector,            # constraint types
+    sos::Vector{SOS}=SOS[]     # SOS information
+)
+    open(filename, "w") do io
+        writemps(io, A, collb, colub, c, rowlb, rowub, sense, colcat, sos)
+    end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,79 @@
-using MPSWriter
-using Base.Test
+using MPSWriter, FactCheck
 
-# write your own tests here
-@test 1 == 1
+facts("getrowsense") do
+    # LE, GE, Eq, Ranged
+    row_sense, hasranged = MPSWriter.getrowsense([-Inf, 0.], [0., Inf])
+    @fact row_sense --> [:(<=), :(>=)]
+    @fact hasranged --> false
+
+    row_sense, hasranged = MPSWriter.getrowsense([1., -1.], [1., 1.])
+    @fact row_sense --> [:(==), :ranged]
+    @fact hasranged --> true
+
+    @fact_throws MPSWriter.getrowsense([1.], [1., 1.])
+    @fact_throws MPSWriter.getrowsense([-Inf], [Inf])
+end
+
+facts("writecolumns!") do
+end
+
+facts("writerows!") do
+    io = IOBuffer()
+    MPSWriter.writerows!(io, [:(<=), :(>=), :(==), :ranged])
+    s = takebuf_string(io)
+    @fact s --> "ROWS\n N  OBJ\n L  C1\n G  C2\n E  C3\n E  C4\n"
+    @fact_throws MPSWriter.writerows!(io, [:badsym])
+    close(io)
+end
+
+facts("writerhs!") do
+    io = IOBuffer()
+    MPSWriter.writerhs!(io, [-Inf, -1., 0., 0.], [0., 1., 0., Inf], [:(<=), :ranged, :(==), :(>=)])
+    s = takebuf_string(io)
+    @fact s --> "RHS\n    rhs       C1        0.0\n    rhs       C2        -1.0\n    rhs       C3        0.0\n    rhs       C4        0.0\n"
+    @fact_throws MPSWriter.writeMPS!(io, [-Inf], [0., 1], [:(<=), :(==)])
+    close(io)
+end
+
+facts("writeranges!") do
+    io = IOBuffer()
+    MPSWriter.writeranges!(io, [-Inf, -2.5, 0., 1.], [1., 1., 1., 2.], [:(<=), :ranged, :ranged, :ranged])
+    s = takebuf_string(io)
+    @fact s --> "RANGES\n    rhs       C2        3.5\n    rhs       C3        1.0\n    rhs       C4        1.0\n"
+    close(io)
+end
+
+facts("writebounds!") do
+    io = IOBuffer()
+    # (-Inf, Inf) = FR
+    # (  * , Inf) = PL
+    # (  * ,  x ) = UP
+    # (-Inf,  * ) = MI
+    # (  x ,  * ) = LO
+    MPSWriter.writebounds!(io, [-Inf, 1., 0., -Inf], [Inf, Inf, Inf, 0.])
+
+    s = takebuf_string(io)
+
+    @fact s --> "BOUNDS\n FR BOUNDS    V1\n PL BOUNDS    V2\n LO BOUNDS    V2        1.0\n PL BOUNDS    V3\n UP BOUNDS    V4        0.0\n MI BOUNDS    V4\n"
+    @fact_throws MPSWriter.writebounds!(io, [-Inf], [Inf, 0.])
+    close(io)
+end
+
+facts("boundstring") do
+    @fact MPSWriter.boundstring("FR", 1) --> " FR BOUNDS    V1"
+    @fact MPSWriter.boundstring("MI", 1) --> " MI BOUNDS    V1"
+    @fact MPSWriter.boundstring("PL", 1) --> " PL BOUNDS    V1"
+    @fact_throws MPSWriter.boundstring("LO", 1)
+    @fact_throws MPSWriter.boundstring("UP", 1)
+    @fact_throws MPSWriter.boundstring("badstring", 1)
+    @fact_throws MPSWriter.boundstring("FR", 1, 1.)
+    @fact_throws MPSWriter.boundstring("MI", 1, 1.)
+    @fact_throws MPSWriter.boundstring("PL", 1, 1.)
+    @fact_throws MPSWriter.boundstring("badstring", 1)
+    @fact MPSWriter.boundstring("LO", 1, 1.) --> " LO BOUNDS    V1        1.0"
+    @fact MPSWriter.boundstring("UP", 1, 1.) --> " UP BOUNDS    V1        1.0"
+    @fact MPSWriter.boundstring("LO", 100, 1.) --> " LO BOUNDS    V100      1.0"
+    @fact MPSWriter.boundstring("UP", 100, 1.) --> " UP BOUNDS    V100      1.0"
+end
+
+FactCheck.exitstatus()
